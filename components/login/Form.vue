59257d1fb22f91eeedb783a9fe6526bc632698b4
clayton <crdcastro5@gmail.com>
2024-02-28 21:38:58 -0800

    integrate bootstrap with login form

--- components/login/Form.vue
+++ components/login/Form.vue
@@ -1,25 +1,24 @@
 <template>
-    <form id="loginForm">
-        <div class="form-group">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" v-model="username" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" v-model="password" required>
-        </div>
-        <div class="button-container">
-            <button type="button" @click="submitForm">Login</button>
-        </div>
-    </form>
+    <div class="container">
+        <form id="loginForm">
+            <div class="form-floating mb-3">
+                <label class="form-label" for="username">Username</label>
+                <input class="form-control" placeholder="Username" type="username" id="username" name="username" v-model="username" required>
+            </div>
+            <div class="form-floating mb-3">
+                <label class="form-label" for="password">Password</label>
+                <input class="form-control" placeholder="Password" type="password" id="password" name="password" v-model="password" required>
+            </div>
+            <div class="center-horizontal">
+                <button class="btn btn-primary" type="button" @click="submitForm">Login</button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const username = ref('');
+    const username = ref('');
 const password = ref('');
-
 async function submitForm() {
     try {
         const loginResult = await login(username.value, password.value)
@@ -31,45 +30,9 @@ async function submitForm() {
 }
 </script>
 
-<style scoped>
-form {
-    max-width: 300px;
-    margin: 0 auto;
-    background-color: var(--bg);
-    color: var(--text);
-}
-
-.form-group {
-    margin-bottom: 15px;
-}
-
-label {
-    display: block;
-    margin-bottom: 5px;
-    color: var(--text-light-1); /* Use red for light text */
-}
-
-input {
-    width: 100%;
-    padding: 8px;
-    box-sizing: border-box;
-    border: 1px solid var(--divider-light-2); /* Use light divider */
-}
-
-button {
-    font-size: 1rem;
-    padding: 10px;
-    background-color: var(--primary-accent-1);
-    color: var(--bg);
-    border: none;
-    cursor: pointer;
-}
-
-button:hover {
-    background-color: var(--primary-accent-2); /* Use Medium Blue for hover */
-}
-
-.button-container {
-    text-align: center;
+<style>
+.center-horizontal {
+    position: relative;
+    left: 40%;
 }
 </style>
